b91d9b0ea70cb43871813b6b2d44eddf79af8e88
maxbma <maxbul2002@gmail.com>
2021-08-27 11:23:45 +0300

    Task 2.9: Order overview page

--- web/src/main/java/com/es/phoneshop/web/controller/pages/OrderOverviewPageController.java
+++ web/src/main/java/com/es/phoneshop/web/controller/pages/OrderOverviewPageController.java
@@ -1,9 +1,29 @@
 package com.es.phoneshop.web.controller.pages;
 
+import com.es.core.cart.CartService;
+import com.es.core.cart.CartTotal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/orderOverview")
 public class OrderOverviewPageController {
+
+    @Resource
+    private CartService cartService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getOrderOverview(Model model){
+        Map<Long,Long> cartItemsCopy = new HashMap<>(cartService.getCart().getItems());
+        CartTotal cartTotal = cartService.getCartTotal(cartItemsCopy);
+        model.addAttribute("itemsAmount", cartTotal.getItemsAmount());
+        model.addAttribute("overallPrice", cartTotal.getOverallPrice());
+        return "orderOverview";
+    }
 }
